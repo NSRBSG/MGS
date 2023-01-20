f1809feeae1277c9fdf69c18e2a083b94f200630
NSRBSG <nsrbsg@gmail.com>
2023-01-20 16:53:26 +0900

    디자인 수정 및 코드 리팩토링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,12 +30,22 @@ const ScrollStickyContainer = styled.div.attrs((props) => ({
 const ScrollStickyContent = styled.div`
   position: sticky;
   top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   height: 100vh;
 `;
 
-const ScrollLockupContainer = styled.div`
+const ScrollLockupContainer = styled.div.attrs((props) => ({
+  style: {
+    height: `${props.currentProgress}%`,
+    width: `${props.currentProgress}%`,
+  },
+}))`
   position: absolute;
-  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 100%;
 `;
@@ -74,20 +84,45 @@ const AnimationBox = styled.div.attrs((props) => ({
   },
 }))`
   position: absolute;
-  top: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100vh;
+  height: 100%;
 `;
 
-const AnimationImage = styled.canvas`
+const AnimationImage = styled.canvas.attrs((props) => ({
+  style: {
+    transform: `translateY(${
+      props.currentProgress >= props.blend.startAt
+        ? props.currentProgress <= props.blend.endAt
+          ? ((props.blend.startAt - props.currentProgress) /
+              (props.blend.endAt - props.blend.startAt)) *
+            100
+          : -200
+        : 0
+    }%)`,
+  },
+}))`
   position: absolute;
   top: 0;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   object-fit: cover;
+`;
+
+const AnimationFocusBox = styled.div.attrs((props) => ({
+  style: {
+    width: `${15 * (1 - props.currentProgress)}%`,
+  },
+}))`
+  background-color: black;
+  position: absolute;
+  top: 0;
+  left: ${(props) => props.left};
+  right: ${(props) => props.right};
+  width: 15%;
+  height: 100%;
 `;
 
 const AnimationText = styled.p`
@@ -95,11 +130,19 @@ const AnimationText = styled.p`
   font-size: 2rem;
   font-weight: 800;
   font-family: NanumGothic;
-  color: white;
+  color: ${(props) => props.color};
 
   @media (min-width: 1024px) {
     font-size: 7vw;
   }
+`;
+
+const AnimationBackgroundImage = styled.img`
+  position: absolute;
+  top: ${(props) => props.top};
+  width: ${(props) => props.width};
+  height: 100%;
+  object-fit: ${(props) => props.objectFit};
 `;
 
 const BannerText = styled.p`
@@ -119,9 +162,9 @@ const BannerText = styled.p`
 `;
 
 const Home = () => {
-  const ScrollContainerHeight = [5];
-  const [inkVideoCurrentProgress, setInkVideoCurrentProgress] = useState(0);
-  const [inkVideoShowingProgress, setInkVideoShowingProgress] = useState(0);
+  const ScrollContainerHeight = 20;
+  const [videoCurrentProgress, setVideoCurrentProgress] = useState(0);
+  const [videoShowingProgress, setVideoShowingProgress] = useState(0);
   const [inkVideo, setInkVideo] = useState([]);
   const stickyContentRef = useRef(null);
   const inkVideoRef = useRef(null);
@@ -131,10 +174,7 @@ const Home = () => {
     .drawImage(
       inkVideo[
         Math.round(
-          301 *
-            (inkVideoCurrentProgress * 1.25 > 1
-              ? 1
-              : inkVideoCurrentProgress * 1.25)
+          301 * (videoCurrentProgress * 2 > 1 ? 1 : videoCurrentProgress * 2)
         )
       ],
       0,
@@ -143,18 +183,18 @@ const Home = () => {
 
   useEffect(() => {
     const onScrollEvent = () => {
-      setInkVideoCurrentProgress(
+      setVideoCurrentProgress(
         stickyContentRef.current.offsetTop /
-          (window.innerHeight * ScrollContainerHeight[0])
+          (window.innerHeight * ScrollContainerHeight)
       );
 
-      const { y } = inkVideoRef.current.getBoundingClientRect();
+      const { y } = stickyContentRef.current.getBoundingClientRect();
       const offset = Math.round(window.innerHeight * 0.8);
 
       if (y >= 0 && y <= offset) {
-        setInkVideoShowingProgress(1 - y / offset);
+        setVideoShowingProgress(1 - y / offset);
       } else {
-        setInkVideoShowingProgress(0);
+        setVideoShowingProgress(0);
       }
     };
 
@@ -163,12 +203,11 @@ const Home = () => {
 
       const currentProgress =
         stickyContentRef.current.offsetTop /
-        Math.round(window.innerHeight * ScrollContainerHeight[0]);
+        Math.round(window.innerHeight * ScrollContainerHeight);
 
       const inputImage =
-        Math.round(
-          301 * (currentProgress * 1.25 > 1 ? 1 : currentProgress * 1.25)
-        ) + 1;
+        Math.round(301 * (currentProgress * 2 > 1 ? 1 : currentProgress * 2)) +
+        1;
 
       for (let i = 1; i <= 302; i++) {
         const image = new Image();
@@ -195,57 +234,98 @@ const Home = () => {
     <Container>
       <Section>
         <BannerText>NSRBSG</BannerText>
-        <ScrollContainer height={ScrollContainerHeight[0] * 100}>
+        <ScrollContainer height={ScrollContainerHeight * 100}>
           <ScrollStickyContainer
             currentProgress={
-              inkVideoCurrentProgress >= 0.8
-                ? -5 * (inkVideoCurrentProgress - 1)
-                : inkVideoShowingProgress
+              videoCurrentProgress >= 0.8
+                ? 5 * (1 - videoCurrentProgress)
+                : videoShowingProgress
             }
           >
             <ScrollStickyContent ref={stickyContentRef}>
-              <ScrollLockupContainer>
-                <AnimationImage ref={inkVideoRef} width="1920" height="1080">
+              <AnimationBackgroundImage
+                width="80%"
+                objectFit="contain"
+                src={require('../lib/assets/images/laptop.jpg')}
+              />
+              <ScrollLockupContainer
+                currentProgress={
+                  videoCurrentProgress >= 0.8
+                    ? 50 * (6 - 5 * videoCurrentProgress)
+                    : 100
+                }
+              >
+                <AnimationBackgroundImage
+                  width="90%"
+                  objectFit="contain"
+                  src={require('../lib/assets/images/background.jpg')}
+                />
+                <AnimationImage
+                  ref={inkVideoRef}
+                  width="1920"
+                  height="1080"
+                  blend={{ startAt: 0.5, endAt: 0.7 }}
+                  currentProgress={videoCurrentProgress}
+                >
                   현재 웹 브라우저를 지원하지 않습니다.
                 </AnimationImage>
+                <AnimationFocusBox
+                  left="0"
+                  currentProgress={videoShowingProgress}
+                />
+                <AnimationFocusBox
+                  right="0"
+                  currentProgress={videoShowingProgress}
+                />
                 <AnimationBox
-                  fadeIn={{ startAt: 0.0, endAt: 0.1 }}
-                  fadeOut={{ startAt: 0.15, endAt: 0.2 }}
-                  currentProgress={inkVideoCurrentProgress}
+                  fadeIn={{ startAt: 0.0, endAt: 0.05 }}
+                  fadeOut={{ startAt: 0.1, endAt: 0.125 }}
+                  currentProgress={videoCurrentProgress}
                 >
-                  <AnimationText>
+                  <AnimationText color="white">
                     완전히 새로워진
                     <br />
                     모각소
                   </AnimationText>
                 </AnimationBox>
                 <AnimationBox
-                  fadeIn={{ startAt: 0.2, endAt: 0.3 }}
-                  fadeOut={{ startAt: 0.35, endAt: 0.4 }}
-                  currentProgress={inkVideoCurrentProgress}
+                  fadeIn={{ startAt: 0.125, endAt: 0.175 }}
+                  fadeOut={{ startAt: 0.225, endAt: 0.25 }}
+                  currentProgress={videoCurrentProgress}
                 >
-                  <AnimationText>
+                  <AnimationText color="white">
                     그 누구도 생각지 못한
                     <br />
                     아름다움
                   </AnimationText>
                 </AnimationBox>
                 <AnimationBox
-                  fadeIn={{ startAt: 0.4, endAt: 0.5 }}
-                  fadeOut={{ startAt: 0.55, endAt: 0.6 }}
-                  currentProgress={inkVideoCurrentProgress}
+                  fadeIn={{ startAt: 0.25, endAt: 0.3 }}
+                  fadeOut={{ startAt: 0.35, endAt: 0.375 }}
+                  currentProgress={videoCurrentProgress}
                 >
-                  <AnimationText>열정으로 가득찬 이곳</AnimationText>
+                  <AnimationText color="white">
+                    열정으로 가득찬 이곳
+                  </AnimationText>
+                </AnimationBox>
+                <AnimationBox
+                  fadeIn={{ startAt: 0.375, endAt: 0.425 }}
+                  fadeOut={{ startAt: 0.475, endAt: 0.5 }}
+                  currentProgress={videoCurrentProgress}
+                >
+                  <AnimationText color="white">
+                    아주대학교에서
+                    <br />
+                    절찬리 진행중
+                  </AnimationText>
                 </AnimationBox>
                 <AnimationBox
                   fadeIn={{ startAt: 0.6, endAt: 0.7 }}
                   fadeOut={{ startAt: 0.75, endAt: 0.8 }}
-                  currentProgress={inkVideoCurrentProgress}
+                  currentProgress={videoCurrentProgress}
                 >
-                  <AnimationText>
-                    아주대학교에서
-                    <br />
-                    절찬리 진행중
+                  <AnimationText color="#c5c5c5">
+                    지금 바로 참여하라!
                   </AnimationText>
                 </AnimationBox>
               </ScrollLockupContainer>
